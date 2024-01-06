3cad8f3cfc41a4d202bd2dde38a43e6ee6a992af
489Esraa <asraaalaa2050@gmail.com>
2024-01-05 19:03:19 -0800

    esraaalaa

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 def element_at(my_list, idx):
+    
     if(idx < 0):
       return None
-    if(idx >= len(my_list)):
+    elif(idx >= len(my_list)):
       return None
-    
-    return my_list[idx]
+    else:
+      return my_list[idx]
