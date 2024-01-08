77933b6f849afcfce085ad38a857bdb652b29c19
489Eraa <asraaalaa2050@gmail.com>
2024-01-08 14:43:01 +0200

    esraaalaa

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
- for row in matrix:
-    m= ' '.join(map(str,row))
-    print("{}".format(m))
-    
-    
+    for row in matrix:
+        print(" ".join("{:d}".format(el) for el in row))
