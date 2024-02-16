537e74f93978537170f98b0c73738090173458d9
489Esraa <asraaalaa2050@gmail.com>
2024-02-16 12:40:11 -0800

    esraaalaa

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
 -- 7.insert new row in table
 -- first_table id,name to the row
 -- values the rows 98,best School
-INSERT INTO first_table(id,name)
-VALUES(98,"Best School");
+INSERT INTO first_table(id, name)
+VALUES(98, "Best School");
