1e0f906e76fec2da4328db3717109dee9f248ff8
489Esraa <asraaalaa2050@gmail.com>
2024-02-17 12:16:19 -0800

    esraaalaa

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,7 +2,7 @@
 -- first_table description id INT , name VARCHAR(256)
 -- if the table exist the command fail
 -- if passed the table of the name added
-CREATE TABLE IF NOT ESISTS first_table (
-id int;
+CREATE TABLE IF NOT ESISTS first_table(
+id int,
 name varchar(256)
 );
