f8fd7ddf57e008dcf90e25732fe3cf17b58d5abf
489Esraa <asraaalaa2050@gmail.com>
2024-03-04 10:20:38 +0200

    esraaalaa

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,17 +12,17 @@ class Square:
     - size: private attribute
     """
 
-    def __init__(self, size = 0):
-      """
+    def __init__(self, size=0):
+        """
       the size attribute that define
       must be integer 
       must be > 0
       else raise error
       """
-      self.__size = size
-      if not isinstance(size, int):
-          raise TypeError("size must be an integer")
-      if __size < 0:
-          raise ValueError("the message size must be >= 0")
-      else:
-          self.__size = size
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("the message size must be >= 0")
+        else:
+            self.__size = size
