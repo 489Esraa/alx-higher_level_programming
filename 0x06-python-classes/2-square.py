30ac14198b56334a94a510310abce8b794586a71
489Esraa <asraaalaa2050@gmail.com>
2024-03-04 10:19:54 +0200

    esraaalaa

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,18 +9,20 @@ class Square:
     This class represents a geometric square.
 
     Attributes:
-    size: size of square
+    - size: private attribute
     """
 
-    def __init__(self, size=0):
-        """initilizations
-
-        Args:
-            Self (size): square size
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    def __init__(self, size = 0):
+      """
+      the size attribute that define
+      must be integer 
+      must be > 0
+      else raise error
+      """
+      self.__size = size
+      if not isinstance(size, int):
+          raise TypeError("size must be an integer")
+      if __size < 0:
+          raise ValueError("the message size must be >= 0")
+      else:
+          self.__size = size
