f76642a4e3234fca06f8c1805cb50c3dcec848a7
489Esraa <asraaalaa2050@gmail.com>
2024-03-04 10:37:19 +0200

    esraaalaa

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -25,4 +25,5 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("the message size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
