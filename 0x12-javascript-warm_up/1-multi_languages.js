02064a4889761a39139cb3cd9d79dafc341de446
489Esraa <asraaalaa2050@gmail.com>
2024-04-09 02:38:44 +0200

    esraaalaa

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 // create a constant variable called myVar with the value “JavaScript is amazing”
-let myVar = 'JavaScript is amazing'
-console.log(myVar)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
