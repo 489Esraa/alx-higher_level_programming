9e4306a7be41125a53f4b61dd98540dc20b10b12
489Esraa <asraaalaa2050@gmail.com>
2024-04-09 03:54:22 +0200

    esraaalaa

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);satisfies
+console.log("C is fun") 
+console.log("Python is cool") 
+console.log("JavaScript is amazing") 
